5522659add6179a34b392ca808e5f4915e146412
Agera-One <182347485+Agera-One@users.noreply.github.com>
2026-06-22 14:09:25 +0700

    Inisialisasi Project

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$database = "invoicing";
+$password = "";
+
+$conn = mysqli_connect($hostname, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Koneksi berhasil brooo!";
+}
