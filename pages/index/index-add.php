5a3c4b2ba0942274a3144f8c9ad46a133f1e0f7c
AgeraOne <182347485+Agera-One@users.noreply.github.com>
2026-06-23 16:09:36 +0700

    Add validation for price and reference number

--- pages/index/index-add.php
+++ pages/index/index-add.php
@@ -6,14 +6,37 @@ if ($_SERVER['REQUEST_METHOD' ] === 'POST') {
     $name = $_POST['name'];
     $price = $_POST['price'];
 
-    $sql = "INSERT INTO item (ref_no, name, price) VALUES ('$ref_no', '$name', $price);";
-
-    if (mysqli_query($conn, $sql)) {
-        header("Location: index.php");
-        exit();
-    } else {
-        echo "Error: " . mysqli_error($conn);
+    if ($price < 0) {
+        echo '<script>alert("The price must not be negative.")</script>';
+        echo '<script>window.location.href = "index-add.php";</script>';
+        exit;
     }
+
+    if (!is_numeric($price)) {
+        echo '<script>alert("The price must be a number.")</script>';
+        echo '<script>window.location.href = "index-add.php";</script>';
+        exit;
+    }
+
+
+    $checkRefNo = "SELECT ref_no FROM item WHERE ref_no='$ref_no'";
+    $result = mysqli_query($conn, $checkRefNo);
+
+    if (mysqli_num_rows($result) > 0) {
+        echo '<script>alert("Reference number already exists. Please use a different reference number.")</script>';
+        echo '<script>window.location.href = "index-add.php";</script>';
+        exit;
+    } else {
+        $sql = "INSERT INTO item (ref_no, name, price) VALUES ('$ref_no', '$name', $price);";
+    
+        if (mysqli_query($conn, $sql)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Error: " . mysqli_error($conn);
+        }
+    }
+
 }
 
 ?>
@@ -38,15 +61,15 @@ if ($_SERVER['REQUEST_METHOD' ] === 'POST') {
             <div class="card-body">
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Reference Number</label>
-                    <input name="ref_no" type="text" class="form-control"
+                    <input name="ref_no" type="text" class="form-control" required>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Name</label>
-                    <input name="name" type="text" class="form-control">
+                    <input name="name" type="text" class="form-control" required>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Price</label>
-                    <input name="price" type="text" class="form-control">
+                    <input name="price" type="text" class="form-control" required>
                 </div>
             </div>
             <div class="card-footer">
