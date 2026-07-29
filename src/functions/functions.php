e732665944fc0161d7ac5c30d665b215ff25c121
Agera-One <182347485+Agera-One@users.noreply.github.com>
2026-07-29 15:24:55 +0700

    Creating pagination and search function

--- src/functions/functions.php
+++ src/functions/functions.php
@@ -17,3 +17,41 @@ function generate_code($database, $table, $column, $prefix) {
 
     return sprintf("%s-%s-%04d", $prefix, $date, $number);
 }
+
+function search($search, $where_condition, $columns)
+{
+    $keyword = isset($search) ? $search : '';
+
+    if ($keyword !== '') {
+        foreach ($columns as $column) {
+            $where_condition['OR']["{$column}[~]"] = $keyword;
+        }
+    }
+
+    return $where_condition;
+}
+
+function pagination($database, $page, $table, $column, $where_condition, $join = [])
+{
+    $limit = 10;
+
+    $active_page = isset($page) ? (int)$page : 1;
+    $offset = ($active_page - 1) * $limit;
+
+    if (!empty($join)) {
+        $rows = $database->count($table, $join, $column, $where_condition);
+    } else {
+        $rows = $database->count($table, $column, $where_condition);
+    }
+    
+    $total_page = ceil($rows / $limit);
+
+    return [
+        'limit'        => $limit,
+        'active_page'  => $active_page,
+        'offset'       => $offset,
+        'where'        => $where_condition,
+        'total_page'   => $total_page,
+        'rows'         => $rows,
+    ];
+}
