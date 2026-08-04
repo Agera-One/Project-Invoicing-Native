6f849937d0fe483b7186ac7ad7ec67106c2e0ae4
Agera-One <182347485+Agera-One@users.noreply.github.com>
2026-08-04 08:34:07 +0700

    separating pagination from the main file

--- pages/user/user.php
+++ pages/user/user.php
@@ -125,33 +125,7 @@ $datas = $user->getAll($query_options);
                         </div>
                     </div>
 
-                    <div class="card-footer bg-transparent border-top d-flex justify-content-end p-3">
-                        <nav aria-label="Page navigation example" class="m-0">
-                            <ul class="pagination pagination-sm m-0">
-                                <?php if ($active_page > 1): ?>
-                                    <li class="page-item">
-                                        <a class="page-link" href="?page=<?= $active_page - 1 ?><?= !empty($search) ? '&search=' . urlencode($search) : '' ?>">Previous</a>
-                                    </li>
-                                <?php else: ?>
-                                    <li class="page-item disabled"><span class="page-link">Previous</span></li>
-                                <?php endif; ?>
-
-                                <?php for ($i = 1; $i <= $total_page; $i++): ?>
-                                    <li class="page-item <?= ($i == $active_page) ? 'active' : '' ?>">
-                                        <a class="page-link" href="?page=<?= $i ?><?= !empty($search) ? '&search=' . urlencode($search) : '' ?>"><?= $i ?></a>
-                                    </li>
-                                <?php endfor; ?>
-
-                                <?php if ($active_page < $total_page): ?>
-                                    <li class="page-item">
-                                        <a class="page-link" href="?page=<?= $active_page + 1 ?><?= !empty($search) ? '&search=' . urlencode($search) : '' ?>">Next</a>
-                                    </li>
-                                <?php else: ?>
-                                    <li class="page-item disabled"><span class="page-link">Next</span></li>
-                                <?php endif; ?>
-                            </ul>
-                        </nav>
-                    </div>
+                    <?php include_once '../../src/components/pagination.php' ?>
                 </div>
             </div>
         </main>
