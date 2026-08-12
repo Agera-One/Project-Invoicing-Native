876d53d77311059c27bb6ca91521e9b16c19881a
Agera-One <182347485+Agera-One@users.noreply.github.com>
2026-08-12 23:34:57 +0700

    Update company model

--- app/models/Company.php
+++ app/models/Company.php
@@ -27,30 +27,45 @@ class Company extends BaseModel {
         ]);
     }
 
-    public function update($id, $datas, $section) {
-        if ($section === 'info') {
-            return $this->getConnection()->update('company', [
-                'name'             => $datas['name'],
-                'business_entity'  => $datas['business_entity'],
-                'sector'           => $datas['sector'],
-                'website'          => $datas['website'],
-                'description'      => $datas['description'],
-                'country'          => $datas['country'],
-                'province'         => $datas['province'],
-                'city'             => $datas['city'],
-                'subdistrict'      => $datas['subdistrict'],
-                'address'          => $datas['address']
-            ], [
-                'id' => $id
-            ]);
-            
-        } elseif ($section === 'contact') {
-            return $this->getConnection()->update('company', [
-                'email' => $datas['email'],
-                'phone' => $datas['phone']
-            ], [
-                'id' => $id
-            ]);
-        }
+    public function updateInfo($id, $datas) {
+        $this->getConnection()->update('company', [
+            'name'             => $datas['name'],
+            'business_entity'  => $datas['business_entity'],
+            'sector'           => $datas['sector'],
+            'website'          => $datas['website'],
+            'description'      => $datas['description'],
+            'country'          => $datas['country'],
+            'province'         => $datas['province'],
+            'city'             => $datas['city'],
+            'subdistrict'      => $datas['subdistrict'],
+            'address'          => $datas['address']
+        ], [
+            'id' => $id
+        ]);
+    }
+
+    public function updateContact($id, $datas) {
+        $this->getConnection()->update('company', [
+            'email' => $datas['email'],
+            'phone' => $datas['phone']
+        ], [
+            'id' => $id
+        ]);
+    }
+
+    public function uploadLogo($id, $new_logo_name) {
+        $this->getConnection()->update('company', [
+            'logo' => $new_logo_name
+        ], [
+            'id' => $id
+        ]);
+    }
+
+    public function uploadSignature($id, $new_signature_name) {
+        $this->getConnection()->update('company', [
+            'signature' => $new_signature_name
+        ], [
+            'id' => $id
+        ]);
     }
 }
