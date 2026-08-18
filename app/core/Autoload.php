d89ba6e4c0598ac0c7bcde1091d0ee5d8495e466
Agera-One <182347485+Agera-One@users.noreply.github.com>
2026-08-18 09:31:59 +0700

    register additional class directories for autoloading

--- app/core/Autoload.php
+++ app/core/Autoload.php
@@ -1,10 +1,22 @@
 <?php
-function load_core($class_name){
-    $path_to_file = '../app/core/' . $class_name . '.php';
 
-    if (file_exists($path_to_file)){
-        require_once ($path_to_file);
+function load_class($class_name)
+{
+    $directories = [
+        '../app/core/',
+        '../app/controllers/',
+        '../app/models/',
+        '../app/services/',
+    ];
+
+    foreach ($directories as $directory) {
+        $path_to_file = $directory . $class_name . '.php';
+
+        if (file_exists($path_to_file)) {
+            require_once($path_to_file);
+            return;
+        }
     }
 }
 
-spl_autoload_register('load_core');
+spl_autoload_register('load_class');
